18436674f7d3c0f3369c5c54db23881199bed6e2
Tahmid Chowdhury <32552650+tahmid-chowdhury@users.noreply.github.com>
2018-09-28 13:47:09 -0400

    Create PS_Output.java

---
+++ java/daily/september/PS_Output.java
@@ -0,0 +1,21 @@
+/*PS_Output
+ * Tahmid Chowdhury
+ * 9/19/2018
+ * 
+ */
+ public class Output_TahmidChowdhury {
+
+	public static void main(String[] args) {
+  
+    //Prints PHS's address for letters
+    System.out.println("Pickering High School\n\n180 Church Street North,\nAjax, ON\nL1T 2W7 Canada");
+
+    //Prints short dialogue
+    System.out.println("Laurie: So, what are your plans for this weekend?\n\nChristie: I don’t know. Do you want to get together or something?\n\nSarah: How about going to see a movie?\n\nLaurie: That sounds like a good idea.");		
+
+    //Creates an ASCII cat
+    System.out.println("_._     _,-'\"\"`-._\r\n" + "(,-.`._,'(       |\\`-/|\r\n" + "   `-.-' \\ )-`( , o o)\r\n" + "         `-    \\`_`\"'-");
+
+  }
+	
+}
