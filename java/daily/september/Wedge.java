6a3f4b05e9e170b8476303a750efea6f6305f120
Tahmid Chowdhury <32552650+tahmid-chowdhury@users.noreply.github.com>
2018-09-28 13:53:42 -0400

    Create Wedge.java

---
+++ java/daily/september/Wedge.java
@@ -0,0 +1,15 @@
+/*Wedge
+ * Tahmid Chowdhury
+ * 9/19/2018
+ * 
+ */
+public class Wedge {
+
+	public static void main(String[] args) {
+  
+    //Prints wedge of stars
+    System.out.println("*******\r\n" + "******\r\n" + "*****\r\n" + "****\r\n" + "***\r\n" + "**\r\n" + "* ");
+    
+	}
+
+}
